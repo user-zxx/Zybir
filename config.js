e9080eda3c7a789e60f2d4f0860846be9f73c710
user-zxx <140407859+user-zxx@users.noreply.github.com>
2023-07-25 20:39:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "上点颜色？",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
