aec9fd2bd5abc747cdce1367773c19d1b37ebd64
user-zxx <140407859+user-zxx@users.noreply.github.com>
2023-07-25 17:02:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给小曾",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "愿贪吃不胖",  // 同上...
+        "祝贪吃不胖",  // 同上...
         "愿好运",
         "岁月悠久",
         "JUST HAPPY",
@@ -20,18 +19,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        //"心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "上点颜色？",
+        bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "有话想对你说",
     }
 };
